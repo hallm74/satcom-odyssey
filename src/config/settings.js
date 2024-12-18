1bbfa23f9775a8930407898ff9d5c3b5de190ced
hallm74 <68484824+hallm74@users.noreply.github.com>
2024-12-17 18:44:32 -0600

    config changes

--- src/config/settings.js
+++ src/config/settings.js
@@ -4,5 +4,5 @@ export default {
 	url: `https://satcom75.netlify.app`, // No trailing slash!
 	name: `SatCom`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
