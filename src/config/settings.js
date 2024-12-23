b461318ee91e5fb2dceecba9b3b492e09887b478
hallm74 <68484824+hallm74@users.noreply.github.com>
2024-12-23 13:54:08 -0600

    changes to meta title and description

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	title: `SatCom | Expert IT Solutions for Businesses and Government Agencies`,
+	description: `SatCom provides comprehensive IT services, including managed IT, security systems, networking, and web design, tailored to businesses and government agencies. Let us simplify your technology needs.`,
 	url: `https://satcom75.netlify.app`, // No trailing slash!
 	name: `SatCom`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
