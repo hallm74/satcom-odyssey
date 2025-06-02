e4961a20cd08eee060ce0158b34956054604ff22
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-06-01 19:05:49 -0500

    going live

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `SatCom | Expert IT Solutions for Businesses and Government Agencies`,
 	description: `SatCom provides comprehensive IT services, including managed IT, security systems, networking, and web design, tailored to businesses and government agencies. Let us simplify your technology needs.`,
-	url: `https://satcom75.netlify.app`, // No trailing slash!
+	url: `https://1satcom.com`, // No trailing slash!
 	name: `SatCom`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
